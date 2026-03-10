afd93e33340c10587df0983af3eefc6a2446b10b
T-eight211 <taher.ahmed2004@hotmail.com>
2026-03-10 14:40:16 +0000

    Add additional block addition logic in GrpcClient.java for matrix operations

--- src/main/java/client/GrpcClient.java
+++ src/main/java/client/GrpcClient.java
@@ -44,7 +44,16 @@ public class GrpcClient {
           .setB10(B[3][0])
           .setB11(B[3][1])
           .build());
-	
+	MatrixReply A3=stub.addBlock(MatrixRequest.newBuilder()
+          .setA00(A3M1.getC00())
+          .setA01(A3M1.getC01())
+          .setA10(A3M1.getC10())
+          .setA11(A3M1.getC11())
+          .setB00(A3M2.getC00())
+          .setB01(A3M2.getC01())
+          .setB10(A3M2.getC10())
+          .setB11(A3M2.getC11())
+          .build());
 	MatrixReply B3M1=stub.multiplyBlock(MatrixRequest.newBuilder()//Second Result Block Calculation
           .setA00(A[0][0])
           .setA01(A[0][1])
@@ -65,7 +74,16 @@ public class GrpcClient {
           .setB10(B[3][2])
           .setB11(B[3][3])
           .build());
-  
+        MatrixReply B3=stub.addBlock(MatrixRequest.newBuilder()
+          .setA00(B3M1.getC00())
+          .setA01(B3M1.getC01())
+          .setA10(B3M1.getC10())
+          .setA11(B3M1.getC11())
+          .setB00(B3M2.getC00())
+          .setB01(B3M2.getC01())
+          .setB10(B3M2.getC10())
+          .setB11(B3M2.getC11())
+          .build());
 	MatrixReply C3M1=stub.multiplyBlock(MatrixRequest.newBuilder()//Third Result Block Calculation
           .setA00(A[2][0])
           .setA01(A[2][1])
@@ -86,7 +104,16 @@ public class GrpcClient {
           .setB10(B[3][0])
           .setB11(B[3][1])
           .build());
-       
+        MatrixReply C3=stub.addBlock(MatrixRequest.newBuilder()
+          .setA00(C3M1.getC00())
+          .setA01(C3M1.getC01())
+          .setA10(C3M1.getC10())
+          .setA11(C3M1.getC11())
+          .setB00(C3M2.getC00())
+          .setB01(C3M2.getC01())
+          .setB10(C3M2.getC10())
+          .setB11(C3M2.getC11())
+          .build());
 	MatrixReply D3M1=stub.multiplyBlock(MatrixRequest.newBuilder()//Fourth Result Block Calculation
           .setA00(A[2][0])
           .setA01(A[2][1])
@@ -107,7 +134,16 @@ public class GrpcClient {
           .setB10(B[3][2])
           .setB11(B[3][3])
           .build());
-     
+        MatrixReply D3=stub.addBlock(MatrixRequest.newBuilder()
+          .setA00(D3M1.getC00())
+          .setA01(D3M1.getC01())
+          .setA10(D3M1.getC10())
+          .setA11(D3M1.getC11())
+          .setB00(D3M2.getC00())
+          .setB01(D3M2.getC01())
+          .setB10(D3M2.getC10())
+          .setB11(D3M2.getC11())
+          .build());
 	System.out.println("Final Answer");
 	System.out.println(A3.getC00()+" "+A3.getC01()+" "+B3.getC00()+" "+B3.getC01());
 	System.out.println(A3.getC10()+" "+A3.getC11()+" "+B3.getC10()+" "+B3.getC11());
